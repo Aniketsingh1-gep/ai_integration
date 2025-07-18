74047b07e8d42cc4a21c583404cde75ace057c57
Aniket Singh <aniket.singh1@gep.com>
2025-07-18 18:16:16 +0530

    test

--- grid-numeric-lib/libs/grid-numeric/src/lib/grid-numeric-interface.ts
+++ grid-numeric-lib/libs/grid-numeric/src/lib/grid-numeric-interface.ts
@@ -7,25 +7,4 @@ export interface INumeric {
         onChange?: any,
         onBlur?: any
     }
-    property1?: any,                
-    property2?: any
-
-    testPropety1?: any,
-    testProperty2?: any,
-
-    propertyAddDemo?: any,
-    propertyAddDemo2?: any,
-
-    demo1: any,
-    demo2: any,
-
-    test1: any,
-    test2: any,
-
-
-    test3: any,
-    test4: any,
-
-    test5: any,
-    test6: any,
 }
